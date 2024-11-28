cf92d8972ef79ddfaf841d0aeecc96da9e6804ea
Thuan Tran <tranthuan12002@gmail.com>
2024-11-28 16:15:10 +0200

    remove unnecessary comments

--- components/InputHandler.js
+++ components/InputHandler.js
@@ -1,13 +1,11 @@
-export default class InputHandler {
+export class InputHandler {
   constructor(game) {
     this.game = game
     window.addEventListener('keydown', (event) => {
       this.game.lastKey = `P ${event.key}`
-      // console.log(this.game.lastKey)
     })
     window.addEventListener('keyup', (event) => {
       this.game.lastKey = `R ${event.key}`
-      // console.log(this.game.lastKey)
     })
   }
 }
